aa3600b68f57b2166095b5d75ab3765ea5074849
Your Name <garuglieritommaso@gmail.com>
2017-11-30 09:59:50 +0100

    added explicit response close on http client

--- providers/torrentz.go
+++ providers/torrentz.go
@@ -48,12 +48,15 @@ func (t Torrentz) getRoot(url string) (*html.Node, error) {
 		log.Fatalln(err)
 	}
 
+	req.Close = true
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
 
 	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
+
+	defer resp.Body.Close()
 
 	root, err := html.Parse(resp.Body)
 	if err != nil {

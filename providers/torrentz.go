320b08f9fafb2b471338c76a1fecc56c8690f5f9
Your Name <garuglieritommaso@gmail.com>
2017-11-30 12:10:02 +0100

    Added request.Close = true on http client

--- providers/torrentz.go
+++ providers/torrentz.go
@@ -41,7 +41,8 @@ func (t Torrentz) getRoot(httpClient http.Client, url string) (*html.Node, error
 		log.Fatalln(err)
 	}
 
-	// req.Close = true
+	req.Close = true
+
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
 
 	resp, err := httpClient.Do(req)
